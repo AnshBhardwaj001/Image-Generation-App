f454c6936a78d3c860d69f5078ef8101c34f1478
Ansh Bhardwaj <123256833+AnshBhardwaj001@users.noreply.github.com>
2024-08-15 06:14:11 +0530

    Dalle 2.0

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -17,7 +17,7 @@ cloudinary.config({
 router.route('/').get(async (req, res) => {
     try{
         const posts = await Post.find({})
-        res.status(500).json({success:true , data:posts})
+        res.status(200).json({success:true , data:posts})
     }catch(error){
         res.status(500).json({success:false , message:error})
     }
